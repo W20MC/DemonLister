ed59d340781e462b1c3aa7f9fc561f86f8aaead8
W20MC <157727813+W20MC@users.noreply.github.com>
2024-02-25 18:42:55 +0100

    Fix an issue relating to content type

---
+++ api.py
@@ -0,0 +1,18 @@
+import aiohttp
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+@router.get("/api/demons/{demon_list}", tags = ["API"])
+async def demons(demon_list):
+    pointercrate_base = "/api/v2/demons/listed/"
+
+    async with aiohttp.ClientSession() as session:
+        if demon_list == "pointercrate":
+            async with session.get(f"https://pointercrate.com{pointercrate_base}") as p:
+                return await p.json(content_type = None)
+        elif demon_list == "challenge":
+            async with session.get(f"https://challengelist.gd{pointercrate_base}") as ch:
+                return await ch.json()
+        else:
+            raise HTTPException(status_code = 404, detail = "Demon list not found. The only accepted values are pointercrate and challenge.")
